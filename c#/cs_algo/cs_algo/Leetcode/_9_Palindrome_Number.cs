352c3504d6bce48fab3c078fa05e454195a07aee
Vadim Kosarev <mailkosarev@gmail.com>
2024-05-27 01:39:20 +0300

    added 9_Palindrome_Number problem in c#

---
+++ c#/cs_algo/cs_algo/Leetcode/_9_Palindrome_Number.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_algo.Leetcode
+{
+    internal static class _9_Palindrome_Number
+    {
+        public static bool IsPalindrome(int x)
+        {
+            bool result = false;
+            if (x < 0) { return false; }
+            else if (x == 0) { return true; }
+            else
+            {
+                string text = x.ToString();
+                string mirror = String.Empty;
+                foreach (char ch in text)
+                {
+                    mirror = ch + mirror;
+                }
+                result = long.Parse(mirror) == (long)x;
+            }
+
+            return result;
+        }
+    }
+}
